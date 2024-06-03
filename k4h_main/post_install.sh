f5b51efc13d8d1fc231a0e5b09afd0d849aea729
Bryan Bliewert <bryan.bliewert@nventis.eu>
2024-06-03 22:54:09 +0200

    Updates

--- k4h_main/post_install.sh
+++ k4h_main/post_install.sh
@@ -3,7 +3,7 @@
 conda activate py311
 pip install ipywidgets
 
-# LCIO
+# Clone and add to python env: LCIO
 echo "Setting up LCIO"
 
 mkdir -p $HOME/ILCSoft/LCIO/build
@@ -16,10 +16,7 @@ make -j 4 install
 ln -s /root/ILCSoft/LCIO/build/_deps/sio_extern-src/python $HOME/ILCSoft/LCIO/python
 conda env config vars set LD_LIBRARY_PATH=$HOME/ILCSoft/LCIO/build/lib64
 
-cat >> $HOME/miniforge3/envs/py311/lib/python3.11/site-packages/lcio.pth <<EOF
-${HOME}/ILCSoft/LCIO/python
-
-EOF
+echo "${HOME}/ILCSoft/LCIO/python" >> $HOME/miniforge3/envs/py311/lib/python3.11/site-packages/lcio.pth
 
 # Link directories
 mkdir -p /pnfs/desy.de/ilc/prod/ilc/mc-2020/ild/dst-merged
@@ -27,3 +24,20 @@ ln -s /nfs/dust/ilc/user/bliewert/500-TDR_ws/ /pnfs/desy.de/ilc/prod/ilc/mc-2020
 
 # bashrc commands
 echo "alias sk4h='source /cvmfs/ilc.desy.de/key4hep/key4hep_latest_setup.sh'" >> $HOME/.bashrc
+
+# Clone and add to python env: ZHH
+echo "Setting up LCIO"
+
+mkdir -p "${HOME}/public/MarlinWorkdirs"
+cd "${HOME}/public/MarlinWorkdirs"
+
+git clone https://github.com/nVentis/ZHH.git
+echo "${HOME}/public/MarlinWorkdirs/ZHH" >> $HOME/miniforge3/envs/py311/lib/python3.11/site-packages/zhh.pth
+
+# Clone and add to python env: pyhepcommon
+echo "Setting up pyhepcommon"
+
+cd "${HOME}/public/MarlinWorkdirs"
+
+git clone https://github.com/nVentis/pyhepcommon.git
+echo "${HOME}/public/MarlinWorkdirs/pyhepcommon" >> $HOME/miniforge3/envs/py311/lib/python3.11/site-packages/pyhepcommon.pth
